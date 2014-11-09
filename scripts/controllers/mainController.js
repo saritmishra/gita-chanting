67dc12c67a346e501bd9212ec567b5c3f705df73
Sarit Mishra <sarit.mishra@gmail.com>
2014-11-08 20:17:50 -0800

    Using verses text instead of image

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -2,10 +2,11 @@
 
 "use strict";
 
-var mainController = function ($routeParams) {
+var mainController = function ($routeParams, $http) {
     // var this = $scope.this = {};
     this.chapterId = $routeParams.chapterId;
     this.verseId = $routeParams.verseId;
+    this.chapterText = [];
 
     // var init = function(){
     if (this.verseId.length === 1)
@@ -14,16 +15,25 @@ var mainController = function ($routeParams) {
         this.chapterId = "0" + this.chapterId;
     // }; init();
 
-    var baseAudioUrl = "data/audio/";
+    var baseAudioUrl = "data/audio/chapter" + this.chapterId + "/";
     this.verseURL = baseAudioUrl + "bvg" + this.chapterId + "v" + this.verseId + ".mp3"; //bvg02v65.mp3
 
-    var baseTextUrl = "data/text/chapter" + this.chapterId + "/";
-    this.verseText = baseTextUrl + "verse-" + this.chapterId + "-" + this.verseId + "-1.png"; //verse-01-38-1.png
+    // var baseTextUrl = "data/text/chapter" + this.chapterId + "/";
+    // this.verseText = baseTextUrl + "verse-" + this.chapterId + "-" + this.verseId + "-1.png"; //verse-01-38-1.png
+
+    var self = this;
+    $http.get("data/text/" + self.chapterId + ".json").
+      success(function(data, status, headers, config) {
+        self.verseText = data[self.verseId - 1] ;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("error in fetching json");
+      })
 
 
 };
 
-angular.module("learn2chant").controller("mainController", [ "$routeParams", mainController]);
+angular.module("learn2chant").controller("mainController", [ "$routeParams", "$http", mainController]);
 
 
 
